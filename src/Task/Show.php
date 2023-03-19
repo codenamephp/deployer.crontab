11a524d54de378322eaf2c28b080881381f5a600
Bastian Schwarz <bastian@repareo.de>
2023-03-19 12:21:20 +0100

    Implemented show task

---
+++ src/Task/Show.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace de\codenamephp\deployer\crontab\Task;
+
+use de\codenamephp\deployer\base\task\iTaskWithDescription;
+use de\codenamephp\deployer\base\task\iTaskWithName;
+
+/**
+ * Lists the crontab ... should have been called 'List' but that is a reserved word
+ *
+ * @psalm-api
+ */
+final class Show extends AbstractCrontabCommand implements iTaskWithName, iTaskWithDescription {
+
+  public const NAME = 'crontab:show';
+
+  public function getOptions() : array {
+    return ['-l'];
+  }
+
+  public function getDescription() : string {
+    return 'Shows the crontab';
+  }
+
+  public function getName() : string {
+    return self::NAME;
+  }
+}
